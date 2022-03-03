2112ed421e629d47e250d0e3c5df4befa2e05f46
黄腾毅 <550188448@qq.com>
2022-03-03 09:29:46 +0800

    修改了gittest

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,8 +1,8 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git 1111");
-        System.out.println("hello git 1111");
-        System.out.println("hello git 1111");
+        System.out.println("hello git 222");
+        System.out.println("hello git 2222");
         System.out.println("hello git 1111");
         System.out.println("hello git 1111");
         System.out.println("hello git 1111");
