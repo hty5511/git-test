c9445f4d3b0ebaa0160b8b104caa304f72ecdbce
黄腾毅 <550188448@qq.com>
2022-03-03 09:28:19 +0800

    第一次提交

---
+++ src/GitTest.java
@@ -0,0 +1,10 @@
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git 1111");
+        System.out.println("hello git 1111");
+        System.out.println("hello git 1111");
+        System.out.println("hello git 1111");
+        System.out.println("hello git 1111");
+        System.out.println("hello git 1111");
+    }
+}
---
+++ src/GitTestWorkSpace.java
@@ -0,0 +1,7 @@
+public class GitTestWorkSpace {
+
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+    }
+
+}
